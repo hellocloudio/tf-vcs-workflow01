e0a0c63dd8574f34d0969aec843a46b735a6c17f
sai <sailinnthu@gmail.com>
2025-02-15 18:14:05 +0800

    Initial files

---
+++ backend.tf
@@ -0,0 +1,11 @@
+# terraform {
+#   cloud {
+#     organization = "hellocloud-tfc-jpn"
+#     ## Required for Terraform Enterprise; Defaults to app.terraform.io for HCP Terraform
+#     hostname = "app.terraform.io"
+
+#     workspaces {
+#       name = "hellocloud-vpc"
+#     }
+#   }
+# }
